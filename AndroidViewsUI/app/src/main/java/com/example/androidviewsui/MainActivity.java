61774e32f0b63d554e79f3e36c836e3232aa4d6d
Yousef T Y Y A Mandani <83332@kfh.com>
2024-01-14 23:42:15 +0300

    adding extra stuff

--- AndroidViewsUI/app/src/main/java/com/example/androidviewsui/MainActivity.java
+++ AndroidViewsUI/app/src/main/java/com/example/androidviewsui/MainActivity.java
@@ -14,81 +14,77 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-    ArrayList<String> questionsList = new ArrayList<String>(Arrays.asList("2 + 2 = 4", "1 + 1 = 11", "2 + 3 = 5"));
-    ArrayList<Boolean> answersList = new ArrayList<Boolean>(Arrays.asList(true, false, true));
+    private ArrayList<String> questionsList = new ArrayList<String>(Arrays.asList("2 + 2 = 4", "1 + 1 = 11", "2 + 3 = 5"));
+    private ArrayList<Boolean> answersList = new ArrayList<Boolean>(Arrays.asList(true, false, true));
 
-    TextView questionText, answerText, scoreText;
-    FrameLayout circle;
-    Drawable greenCircle;
-    Drawable redCircle;
+    private TextView questionText, answerText, scoreText;
+    private FrameLayout circle;
+    private Drawable greenCircle, redCircle;
 
-    Button nextButton, trueButton, falseButton;
-    int index = 0;
-    static int score = 0;
+    private Button nextButton, trueButton, falseButton;
+    private int index = 0;
+    private static int score = 0;
+
+    public void initialize(){
+        greenCircle = ResourcesCompat.getDrawable(getResources(), R.drawable.green_circle, null);
+        redCircle = ResourcesCompat.getDrawable(getResources(), R.drawable.red_circle, null);
+
+        questionText = findViewById(R.id.questionTextView);
+        scoreText = findViewById(R.id.scoreText);
+        circle = findViewById(R.id.frameLayout);
+        answerText = findViewById(R.id.answerText);
+        nextButton = findViewById(R.id.nextButton);
+        falseButton = findViewById(R.id.falseButton);
+        trueButton = findViewById(R.id.trueButton);
+    }
+
+    public void correctAnswer(){
+        score++;
+        scoreText.setText("Score: " + score);
+        index++;
+        circle.setBackground(greenCircle);
+        circle.setVisibility(View.VISIBLE);
+        answerText.setVisibility(View.VISIBLE);
+        answerText.setText("Correct");
+        trueButton.setVisibility(View.INVISIBLE);
+        falseButton.setVisibility(View.INVISIBLE);
+        nextButton.setVisibility(View.VISIBLE);
+    }
+
+    public void incorrectAnswer(){
+        circle.setBackground(redCircle);
+        circle.setVisibility(View.VISIBLE);
+        answerText.setVisibility(View.VISIBLE);
+        answerText.setText("Incorrect");
+        trueButton.setVisibility(View.INVISIBLE);
+        falseButton.setVisibility(View.INVISIBLE);
+        nextButton.setText("Try Again");
+        nextButton.setVisibility(View.VISIBLE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        greenCircle = ResourcesCompat.getDrawable(getResources(), R.drawable.green_circle, null);
-        redCircle = ResourcesCompat.getDrawable(getResources(), R.drawable.red_circle, null);
+        initialize();
 
-        questionText = findViewById(R.id.questionTextView);
         questionText.setText(questionsList.get(index));
-
-        scoreText = findViewById(R.id.scoreText);
         scoreText.setText("Score: " + score);
 
-        circle = findViewById(R.id.frameLayout);
-        answerText = findViewById(R.id.answerText);
-        nextButton = findViewById(R.id.nextButton);
-        falseButton = findViewById(R.id.falseButton);
-        trueButton = findViewById(R.id.trueButton);
-
         falseButton.setOnClickListener(v -> {
-            if(answersList.get(index) == false){
-                score++;
-                scoreText.setText("Score: " + score);
-                index++;
-                circle.setBackground(greenCircle);
-                circle.setVisibility(View.VISIBLE);
-                answerText.setVisibility(View.VISIBLE);
-                answerText.setText("Correct");
-                trueButton.setVisibility(View.INVISIBLE);
-                falseButton.setVisibility(View.INVISIBLE);
-                nextButton.setVisibility(View.VISIBLE);
+            if(!answersList.get(index)){
+                correctAnswer();
             } else {
-                circle.setBackground(redCircle);
-                circle.setVisibility(View.VISIBLE);
-                answerText.setVisibility(View.VISIBLE);
-                answerText.setText("Incorrect");
-                trueButton.setVisibility(View.INVISIBLE);
-                falseButton.setVisibility(View.INVISIBLE);
-                nextButton.setVisibility(View.VISIBLE);
+                incorrectAnswer();
             }
         });
 
         trueButton.setOnClickListener(v ->{
-            if(answersList.get(index) == true){
-                score++;
-                scoreText.setText("Score: " + score);
-                index++;
-                circle.setBackground(greenCircle);
-                circle.setVisibility(View.VISIBLE);
-                answerText.setVisibility(View.VISIBLE);
-                answerText.setText("Correct");
-                trueButton.setVisibility(View.INVISIBLE);
-                falseButton.setVisibility(View.INVISIBLE);
-                nextButton.setVisibility(View.VISIBLE);
+            if(answersList.get(index)){
+                correctAnswer();
             } else {
-                circle.setBackground(redCircle);
-                circle.setVisibility(View.VISIBLE);
-                answerText.setVisibility(View.VISIBLE);
-                answerText.setText("Incorrect");
-                trueButton.setVisibility(View.INVISIBLE);
-                falseButton.setVisibility(View.INVISIBLE);
-                nextButton.setVisibility(View.VISIBLE);
+                incorrectAnswer();
             }
         });
 
@@ -97,14 +93,30 @@ public class MainActivity extends AppCompatActivity {
                 index = 0;
                 score = 0;
                 scoreText.setText("Score: " + score);
+                answerText.setVisibility(View.INVISIBLE);
+                circle.setVisibility(View.INVISIBLE);
+                nextButton.setVisibility(View.INVISIBLE);
+                trueButton.setVisibility(View.VISIBLE);
+                falseButton.setVisibility(View.VISIBLE);
+                questionText.setText(questionsList.get(index));
+            } else if (index == answersList.size() - 1) {
+                nextButton.setText("Reset");
+                answerText.setVisibility(View.INVISIBLE);
+                circle.setVisibility(View.INVISIBLE);
+                nextButton.setVisibility(View.INVISIBLE);
+                trueButton.setVisibility(View.VISIBLE);
+                falseButton.setVisibility(View.VISIBLE);
+                questionText.setText(questionsList.get(index));
+            } else {
+                nextButton.setText("Next Question");
+                answerText.setVisibility(View.INVISIBLE);
+                circle.setVisibility(View.INVISIBLE);
+                nextButton.setVisibility(View.INVISIBLE);
+                trueButton.setVisibility(View.VISIBLE);
+                falseButton.setVisibility(View.VISIBLE);
+                questionText.setText(questionsList.get(index));
             }
 
-            answerText.setVisibility(View.INVISIBLE);
-            circle.setVisibility(View.INVISIBLE);
-            nextButton.setVisibility(View.INVISIBLE);
-            trueButton.setVisibility(View.VISIBLE);
-            falseButton.setVisibility(View.VISIBLE);
-            questionText.setText(questionsList.get(index));
         });
     }
 }
